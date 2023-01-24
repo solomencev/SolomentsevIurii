a4927aa807cb070493be99ea6a344f9109156ca0
iurii solomentsev <emirateskos@gmail.com>
2023-01-24 17:34:37 +0400

    added cucumber-java 7.10.0 and cucumber-testng 7.10.1 in pom; copied all test content from hw4 to hw5;

---
+++ src/test/java/com/epam/tc/hw5/steps/BaseStep.java
@@ -0,0 +1,22 @@
+package com.epam.tc.hw5.steps;
+
+import com.epam.tc.hw4.DifferentElementsPage;
+import com.epam.tc.hw4.MainPage;
+import com.epam.tc.hw4.hw4.fragments.HeaderMenu;
+import com.epam.tc.hw5.AbstractChromeTest;
+import org.assertj.core.api.SoftAssertions;
+import org.openqa.selenium.WebDriver;
+
+public abstract class BaseStep extends AbstractChromeTest {
+
+    SoftAssertions softAssert = new SoftAssertions();
+    public static MainPage mainPage;
+    public static DifferentElementsPage differentElementsPage;
+    public static HeaderMenu headerMenu;
+
+    protected BaseStep(WebDriver webDriver) {
+        mainPage = new MainPage(webDriver);
+        differentElementsPage = new DifferentElementsPage(webDriver);
+        headerMenu = new HeaderMenu(webDriver);
+    }
+}

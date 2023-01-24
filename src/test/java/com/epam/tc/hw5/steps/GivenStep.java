762b4d95dd026d0a8b4a39b632482ab2404b12cb
iurii solomentsev <emirateskos@gmail.com>
2023-01-24 18:55:30 +0400

    Created Given, Then, When steps; RunCucumber runner; created first feature but scenario does not work (NPE)

---
+++ src/test/java/com/epam/tc/hw5/steps/GivenStep.java
@@ -0,0 +1,19 @@
+package com.epam.tc.hw5.steps;
+
+import com.epam.tc.hw5.AbstractChromeTest;
+import io.cucumber.java.en.Given;
+import java.io.IOException;
+import org.openqa.selenium.WebDriver;
+
+public class GivenStep extends AbstractChromeTest {
+
+    @Given("User opens website")
+    public void openWebSite() {
+        actionStep.openWebSite();
+    }
+
+    /*@Given("User opens JDI GitHub site")
+    public void openJdiGitHubWebsite() {
+        actionStep.openWebSite();
+    }*/
+}
---
+++ src/test/resources/features/Exercise1Test.feature
@@ -0,0 +1,6 @@
+Feature: Make some changes on website?
+  test1
+
+  Scenario: test1
+    Given User opens website
+

c5adbadc334d6245d05a48a944711ede5bb72bd3
iurii solomentsev <emirateskos@gmail.com>
2023-02-06 09:54:27 +0400

    Added description and renamed test for HM7;

--- src/test/java/com/epam/tc/hw7/JdiSiteTest.java
+++ src/test/java/com/epam/tc/hw7/JdiSiteTest.java
@@ -2,12 +2,14 @@ package com.epam.tc.hw7;
 
 import static com.epam.tc.hw7.pages.JdiHomePage.ROMAN_IOVLEV;
 
+import io.qameta.allure.Description;
 import org.testng.annotations.Test;
 
 public class JdiSiteTest extends AbstractTest {
 
     @Test(dataProvider = "DataProviderJson", dataProviderClass = DataProviderMetalsAndColors.class)
-    public void jdiHomepageTest(jdi.entities.MetalsAndColors metalsAndColors) {
+    @Description(value = "User can submit Metals & Colors form")
+    public void metalColorsFormSubmittingTest(jdi.entities.MetalsAndColors metalsAndColors) {
         JdiSite.homePage.userName.is().text(ROMAN_IOVLEV.getFullName());
         JdiSite.openMetalsAndColorsPage();
         JdiSite.metalsAndColorsPage.fillMetalAndColorForm(metalsAndColors);
